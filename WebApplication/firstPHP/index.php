1d41a2abb176d2b7847211b4dcc66f3c1bad3600
kinutaISE <81940555+mKenta0325@users.noreply.github.com>
2022-05-11 15:34:57 +0900

    Create index.php

---
+++ WebApplication/firstPHP/index.php
@@ -0,0 +1,20 @@
+<?php
+
+$value = 100 ;
+
+?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<meta charset="utf-8" />
+
+<head>
+  <title>test php</title>
+</head>
+
+<body>
+  <p>これはテストです<br></p>
+  <p>value = <?= $value ; ?></p>
+</body>
+
+</html>
